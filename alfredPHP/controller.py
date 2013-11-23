21aa4868681a4ea7e4e90ef326499da8afdbc009
Aaron Haight <aaron@liquidwms.com>
2013-11-23 12:11:09 -0500

    no message

--- alfredPHP/controller.py
+++ alfredPHP/controller.py
@@ -2,6 +2,8 @@ import smbus, sys
 import time
 # for RPI version 1, use "bus = smbus.SMBus(0)"
 bus = smbus.SMBus(1)
+print sys.argv[1]
+return
 
 # This is the address we setup in the Arduino Program
 address = 0x04
